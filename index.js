adbb33421950020ec5040f9f00254292bd1e1ea6
shahzan01 <m.shahzan2003@gmail.com>
2024-12-23 19:47:24 +0530

    api

--- index.js
+++ index.js
@@ -57,11 +57,11 @@ app.use(
 cloudinaryConnect();
 
 //routes
-app.use("/api/v1/auth", userRoutes);
-app.use("/api/v1/profile", profileRoutes);
-app.use("/api/v1/course", courseRoutes);
-app.use("/api/v1/payment", paymentRoutes);
-app.use("/api/v1/reach", contactUsRoute);
+app.use("/auth", userRoutes);
+app.use("/profile", profileRoutes);
+app.use("/course", courseRoutes);
+app.use("/payment", paymentRoutes);
+app.use("/reach", contactUsRoute);
 
 //def route
 
