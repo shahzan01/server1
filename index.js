94f37424ab172b61b8d632fe6b7d935cb284dadb
shahzan01 <m.shahzan2003@gmail.com>
2024-12-23 19:33:03 +0530

    y

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require("express");
+const app = express();
+
+const userRoutes = require("./routes/User");
+const profileRoutes = require("./routes/Profile");
+const paymentRoutes = require("./routes/Payments");
+const courseRoutes = require("./routes/Course");
+const contactUsRoute = require("./routes/Contact");
+const database = require("./config/database");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const {cloudinaryConnect } = require("./config/cloudinary");
+const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const PORT = process.env.PORT || 4000;
+
+//database connect
+database.connect();
+
+const allowedOrigins = [
+	'https://study-notion-frontend-orcin.vercel.app',
+	'https://study-notion-api-gules.vercel.app',
+  ];
+  
+  // Middlewares
+  app.use(cors({
+	origin: (origin, callback) => {
+	  if (!origin || allowedOrigins.includes(origin)) {
+		callback(null, true);
+	  } else {
+		callback(new Error('Not allowed by CORS'));
+	  }
+	},
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization'],
+	credentials: true,
+  }));
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))//for using postman
+app.use(cookieParser());
+app.use(
+	cors({
+		origin:"http://localhost:3000",
+		credentials:true,
+	})
+)
+
+app.use(
+	fileUpload({
+		useTempFiles:true,
+		tempFileDir:"/tmp",
+	})
+)
+//cloudinary connection
+cloudinaryConnect();
+
+//routes
+app.use("/api/v1/auth", userRoutes);
+app.use("/api/v1/profile", profileRoutes);
+app.use("/api/v1/course", courseRoutes);
+app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/reach", contactUsRoute);
+
+//def route
+
+app.get("/", (req, res) => {
+	return res.json({
+		success:true,
+		message:'Your server is up and running....'
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`App is running at ${PORT}`)
+})
+
---
+++ utils/mailSender.js
@@ -0,0 +1,36 @@
+const nodemailer = require("nodemailer");
+
+const mailSender = async (email, title, body) => {
+    try{
+            let transporter = nodemailer.createTransport({
+                host:"gmail",
+                service:"gmail",
+                
+                 //add certification
+                 port: 465,
+                 secure: true,
+                auth:{
+                    user: process.env.MAIL_USER,
+                    pass: process.env.MAIL_PASS,
+                }
+            })
+
+
+            let info = await transporter.sendMail({
+                from: 'StudyNotion || CodeHelp - by Babbar',
+                to:`${email}`,
+                subject: `${title}`,
+                html: `${body}`,
+            })
+            console.log(info);
+            return info;
+    }
+    catch(error) {
+        console.log(error);
+        //throw right error
+        throw error;
+    }
+}
+
+
+module.exports = mailSender;
